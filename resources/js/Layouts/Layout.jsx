237a8ff41f394808a3a6803c99aa753e8ce0f131
Root <abdullahalmasrur8@gmail.com>
2025-09-25 14:13:57 +0600

    sidebar, layout, progress,

---
+++ resources/js/Layouts/Layout.jsx
@@ -0,0 +1,31 @@
+import { Link } from "@inertiajs/react";
+
+export default function Layout({ children }) {
+    return (
+        <div className="flex gap-10">
+            <nav className="w-1/5 h-dvh bg-blue-900 rounded-e-lg shadow-2xl text-white p-5 flex flex-col gap-3 text-xl">
+                <h1 className="text-3xl underline mb-5">Besnik Test</h1>
+                <Link
+                    className="hover:bg-blue-950 transition-colors ps-2 py-2"
+                    href="/"
+                >
+                    Home
+                </Link>
+                <Link
+                    className="hover:bg-blue-950 transition-colors ps-2 py-2"
+                    href="/"
+                >
+                    Course
+                </Link>
+                <Link
+                    className="hover:bg-blue-950 transition-colors ps-2 py-2"
+                    href="/"
+                >
+                    Student
+                </Link>
+            </nav>
+
+            <main>{children}</main>
+        </div>
+    );
+}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,3 +1,12 @@
-export default function Home() {
-    return <h1 className="text-2xl text-red-800">Hello World</h1>;
+import Layout from "../Layouts/Layout";
+
+function Home() {
+    return (
+        <>
+            <h1 className="text-2xl text-red-800 text-center">Hello World</h1>
+        </>
+    );
 }
+// Home.layout = (page) => <Layout children={page} />;
+
+export default Home;
